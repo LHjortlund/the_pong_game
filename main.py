0ce0e3edcd75a81543c456f43275469b0b06eb92
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-06 17:32:39 +0200

    update paddle

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from turtle import Turtle, Screen
+UP = 90
+DOWN = 270
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("My Pong Game")
+screen.tracer(0)
 
 
+
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+def paddle_up():
+    new_y= paddle.ycor()+20
+    paddle.goto(paddle.xcor(), new_y)
+
+def paddle_down():
+    new_y= paddle.ycor()-20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(paddle.up,"Up")
+screen.onkey(paddle.down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,13 @@
+import turtle
+from turtle import Turtle
+STARTING_POSITIONS = starting_positions = [(350, 0)]
+
+class Paddle:
+    def __init__(self):
+        self.segments = []
+        self.create_paddle()
+
+        turtle.shapesize(stretch_wid=3, stretch_len=1, outline=0)
+        paddle.goto(position)
+
+    def create_paddle(self):
